3fb5d920477ede848634f82c2878b0c3f39149dd
Konstantin Turanosov <kamenkamf@gmail.com>
2025-09-17 20:34:34 +0300

    second

--- Основы_go/Go-demo/main.go
+++ Основы_go/Go-demo/main.go
@@ -8,9 +8,43 @@ import (
 const IMTPower = 2
 
 func main() {
+	// for i := 0; i < 10; i++ {
+	// 	if i == 5 {
+	// 		continue
+	// 	}
+	// 	fmt.Printf("%d\n", i)
+	// }
+
 	fmt.Println("__Калькулятор индекса массы тела__")
 	userHeiht, userWeight := getUserInput()
 	IMT := calculateIMT(userHeiht, userWeight)
+	switch {
+	case IMT < 16:
+		fmt.Println("У Вас сильный недостаток веса")
+	case IMT < 18.5:
+		fmt.Println("Дефицит массы тела")
+	case IMT < 25:
+		fmt.Println("Нормальный вес")
+	case IMT < 30:
+		fmt.Println("Избыточный вес")
+	default:
+		fmt.Println("Степень ожирения")
+
+	}
+
+	// isLean := IMT < 16
+	// if isLean {
+	// 	fmt.Println("У Вас сильный недостаток веса")
+	// } else if IMT < 18.5 {
+	// 	fmt.Println("Дефицит массы тела")
+	// } else if IMT < 25 {
+	// 	fmt.Println("Нормальный вес")
+	// } else if IMT < 30 {
+	// 	fmt.Println("Избыточный вес")
+	// } else {
+	// 	fmt.Println("Степень ожирения")
+	// }
+
 	outputResult(IMT)
 }
 
