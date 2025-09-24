bc3e57276931ef535c4b55a6eb62cb3a6f203022
Konstantin Turanosov <kamenkamf@gmail.com>
2025-09-24 21:12:10 +0300

    second

--- Основы_go/Go-demo/main.go
+++ Основы_go/Go-demo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -8,55 +9,47 @@ import (
 const IMTPower = 2
 
 func main() {
-	// for i := 0; i < 10; i++ {
-	// 	if i == 5 {
-	// 		continue
-	// 	}
-	// 	fmt.Printf("%d\n", i)
-	// }
-
 	fmt.Println("__Калькулятор индекса массы тела__")
-	userHeiht, userWeight := getUserInput()
-	IMT := calculateIMT(userHeiht, userWeight)
-	switch {
-	case IMT < 16:
-		fmt.Println("У Вас сильный недостаток веса")
-	case IMT < 18.5:
-		fmt.Println("Дефицит массы тела")
-	case IMT < 25:
-		fmt.Println("Нормальный вес")
-	case IMT < 30:
-		fmt.Println("Избыточный вес")
-	default:
-		fmt.Println("Степень ожирения")
-
+	for {
+		userHeiht, userWeight := getUserInput()
+		IMT, err := calculateIMT(userHeiht, userWeight)
+		if err != nil {
+			// fmt.Println("Не заданы параметры для рассчета")
+			// continue
+			panic("Не заданы параметры для рассчета")
+		}
+		outputResult(IMT)
+		isRepeateCalculation := checkRepeatCalculation()
+		if !isRepeateCalculation {
+			break
+		}
 	}
-
-	// isLean := IMT < 16
-	// if isLean {
-	// 	fmt.Println("У Вас сильный недостаток веса")
-	// } else if IMT < 18.5 {
-	// 	fmt.Println("Дефицит массы тела")
-	// } else if IMT < 25 {
-	// 	fmt.Println("Нормальный вес")
-	// } else if IMT < 30 {
-	// 	fmt.Println("Избыточный вес")
-	// } else {
-	// 	fmt.Println("Степень ожирения")
-	// }
-
-	outputResult(IMT)
 }
 
 func outputResult(imt float64) {
+	switch {
+	case imt < 16:
+		fmt.Println("У Вас сильный недостаток веса")
+	case imt < 18.5:
+		fmt.Println("Дефицит массы тела")
+	case imt < 25:
+		fmt.Println("Нормальный вес")
+	case imt < 30:
+		fmt.Println("Избыточный вес")
+	default:
+		fmt.Println("Степень ожирения")
+	}
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
 	fmt.Print(result)
 
 }
 
-func calculateIMT(userHeiht float64, userWeight float64) (IMT float64) {
-	IMT = userWeight / math.Pow(userHeiht/100, IMTPower)
-	return
+func calculateIMT(userHeiht float64, userWeight float64) (float64, error) {
+	if userHeiht <= 0 || userWeight <= 0 {
+		return 0, errors.New("No params ERROR")
+	}
+	IMT := userWeight / math.Pow(userHeiht/100, IMTPower)
+	return IMT, nil
 }
 
 func getUserInput() (float64, float64) {
@@ -67,4 +60,14 @@ func getUserInput() (float64, float64) {
 	fmt.Print("Введите Ваш вес: ")
 	fmt.Scan(&userWeight)
 	return userHeiht, userWeight
+}
+
+func checkRepeatCalculation() bool {
+	var userChoise string
+	fmt.Print("\nВы хотите сделать ещё расчет (y/n)")
+	fmt.Scan(&userChoise)
+	if userChoise == "y" || userChoise == "Y" {
+		return true
+	}
+	return false
 }
